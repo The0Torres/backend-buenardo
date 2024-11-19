8b6f319674ad930b0142e1b087b578b0dad4828b
The0Torres <theotocar@gmail.com>
2024-11-19 19:30:47 -0300

    AZ<SDAS

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post("/send-mail", async (req, res) => {
     });
 
     let resultado = await transporter.sendMail({
-      from: '"Sabor Academico" <saboracademico@gmail.com>',
+      from: '"Restaurante Buenardo" <restaurantebuenardo@gmail.com>',
       to: mail,
       subject: aceptacion
         ? "Felicitaciones su cuenta fue aceptada"
@@ -122,7 +122,7 @@ app.post("/send-mail", async (req, res) => {
             <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border: 1px solid #dddddd; padding: 20px; border-radius: 8px; text-align: center;">
               
               <!-- Logo de la empresa -->
-              <img src="https://firebasestorage.googleapis.com/v0/b/tp-clinica-online-5cb54.appspot.com/o/logo.jpeg?alt=media&token=d3b33426-153e-46b2-b521-9f6cf8e10b2f" alt="Logo Sabor Académico" style="width: 100px; margin-bottom: 20px;">
+              <img src="https://firebasestorage.googleapis.com/v0/b/pruebaapp-b4da6.appspot.com/o/photos%2FGroup%2022.png?alt=media&token=773fdd95-b818-4126-88a4-81b30c5cdfce" alt="Logo Sabor Académico" style="width: 100px; margin-bottom: 20px;">
 
               <h1 style="color: ${aceptacion ? '#4CAF50' : '#FFA726'};">
                 ${aceptacion ? "¡Felicitaciones!" : "¡Cuenta creada!"} ${nombreUsuario}
@@ -139,7 +139,7 @@ app.post("/send-mail", async (req, res) => {
               <hr style="border: none; border-top: 1px solid #eeeeee; margin: 20px 0;">
 
               <p style="font-size: 16px; color: #333333;">
-                Saludos, <br> <strong>Sabor Académico</strong>
+                Saludos, <br> <strong>Restaurante Buenardo</strong>
               </p>
             </div>
           </div>
@@ -170,7 +170,7 @@ app.post("/reject-mail", async (req, res) => {
     });
 
     let resultado = await transporter.sendMail({
-      from: '"Sabor Academico" <saboracademico@gmail.com>',
+      from: '"Restaurante Buenardo" <restaurantebuenardo@gmail.com>',
       to: mail,
       subject:
         "Notificacion de rechazo",
@@ -182,7 +182,7 @@ app.post("/reject-mail", async (req, res) => {
           <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border: 1px solid #dddddd; padding: 20px; border-radius: 8px; text-align: center;">
             
             <!-- Logo de la empresa -->
-            <img src="https://firebasestorage.googleapis.com/v0/b/tp-clinica-online-5cb54.appspot.com/o/logo.jpeg?alt=media&token=d3b33426-153e-46b2-b521-9f6cf8e10b2f" alt="Logo Sabor Académico" style="width: 100px; margin-bottom: 20px;">
+            <img src="https://firebasestorage.googleapis.com/v0/b/pruebaapp-b4da6.appspot.com/o/photos%2FGroup%2022.png?alt=media&token=773fdd95-b818-4126-88a4-81b30c5cdfce" alt="Logo Sabor Académico" style="width: 100px; margin-bottom: 20px;">
       
             <h1 style="color: #E53935;">
               Lo sentimos, ${nombreUsuario}
@@ -203,7 +203,7 @@ app.post("/reject-mail", async (req, res) => {
             </p>
       
             <p style="font-size: 16px; color: #333333;">
-              Saludos cordiales, <br> <strong>Sabor Académico</strong>
+              Saludos cordiales, <br> <strong>Restaurante Buenardo</strong>
             </p>
           </div>
         </div>
